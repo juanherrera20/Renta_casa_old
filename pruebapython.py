a93294ec50cd788041414a3f43881a8839b53b60
juanherrera20 <juanuper2@gmail.com>
2024-02-27 21:18:09 -0500

    Esto es una prueba añadida a la branch juan

--- pruebapython.py
+++ pruebapython.py
@@ -1,4 +1,11 @@
 # Esto es un ensayo de push y commits en github
 
 print("Hola mundo!")
-print(2 +2)
+nombres = ["Juan","Pedro","lucas"]
+nombres.append("Sandra")
+
+
+for i in nombres:
+    print(i)
+
+#Estas son otras pruebas para otro comit en una nueva branch
