390b80f34a01ff7d73e933165189e8cce6c76768
Brandon Restrepo <jhon2.restrepo@ucp.edu.co>
2024-03-05 10:37:04 -0500

    corrección e implementación de navegación

--- desarrollo/generar/views.py
+++ desarrollo/generar/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import superuser
 
@@ -12,4 +12,14 @@ def register(request):
     return render(request,'register.html')
 
 def dash(request):
-    return render(request, 'dash.html')
+    if request.method == 'GET': #Condicional para saber si los datos si se enviarón
+        docuemnto = request.GET.get('docuemnto', None) #Recopilación de los datos del formulario
+        password = request.GET.get('password', None)
+        button1 = request.GET.get('entrar')
+        button2 = request.GET.get('registrar')
+        if(button1 == "1"):
+            return render(request, 'dash.html')
+        elif(button2 == "2"):
+             return redirect('register')
+
+    
