6a3ccf07e243cbeafa715a22b488cfd32e1f73a9
Brandon Restrepo <jhon2.restrepo@ucp.edu.co>
2024-04-04 16:47:44 -0500

    Input "Buscar" Completo y funcionando

---
+++ desarrollo/generar/static/js/analisis/analisis_propietarios.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    document.getElementById('buscar').addEventListener('input', function() {
+      var value = this.value.toUpperCase();
+      var rows = document.getElementById('propietarios').getElementsByTagName('tr');
+      for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName('td');
+        var match = false;
+        for (var j = 0; j < cells.length; j++) {
+          if (cells[j].textContent.toUpperCase().indexOf(value) > -1) {
+            match = true;
+            break;
+          }
+        }
+        rows[i].style.display = match ? "" : "none";
+      }
+    });
+  };
